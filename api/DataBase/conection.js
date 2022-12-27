8266f47ad76f650f09a9fd396f6d6582200933f3
unknown <you@example.com>
2022-12-27 19:42:13 +0500

    again change

--- api/DataBase/conection.js
+++ api/DataBase/conection.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+mongoose.set('strictQuery', true);
 mongoose.connect("mongodb+srv://shahmir_99:firecape188@cluster0.pppostr.mongodb.net/chatapp?retryWrites=true&w=majority").then(() => {
     console.log("Connection With mongodb Successfully")
 }).catch((error) => {
